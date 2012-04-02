1ec56adf6ef1f223f7e75ea7b9ad401ac080c89e
Michael Sommer <m.sommer@ogd.nl>
2012-04-02 17:30:10 +0200

    Now when the server is down ELMAH is notified.

---
+++ Source/Ogd.LdapProviders/LdapMembershipProviderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+using System.Web;
+using System.Web.Security;
+
+namespace Ogd.Web.Security
+{
+    public static class LdapMembershipProviderExtensions
+    {
+        public static string GetDisplayName(this MembershipUser user)
+        {
+            return user.UserName.GetDisplayName();
+        }
+
+        public static string GetDisplayName(this string username)
+        {
+            try
+            {
+                using (var context = new PrincipalContext(ContextType.Domain))
+                {
+                    try
+                    {
+                        var principal = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, username);
+                        return principal.DisplayName;
+                    }
+                    catch (Exception ex)
+                    {
+                        Elmah.ErrorLog.GetDefault(HttpContext.Current).Log(new Elmah.Error(ex, HttpContext.Current));
+                        return "";
+                    }
+                }
+            }
+            catch (PrincipalServerDownException ex)
+            {
+                Elmah.ErrorLog.GetDefault(HttpContext.Current).Log(new Elmah.Error(ex, HttpContext.Current));
+                return username;
+            }
+        }
+    }
+}
